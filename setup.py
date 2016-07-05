0da54935c76c782d369c11729ba2a1461ddea756
Ravi Peters <r.y.peters@tudelft.nl>
2016-07-05 13:19:39 +0200

    add igraph dependency

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     name = "pointio",
     version = "0.1",
     packages = find_packages(),
-    install_requires = ["numpy>=1.9.1"],
+    install_requires = ["numpy>=1.9.1", "python-igraph"],
     extras_require = {'las':["laspy"], 'pcd':["pypcd"]},
     scripts = ['util/las2npy.py', 'util/npy2las.py', 'util/npy2pcd.py', 'util/pcd2npy.py', 'util/ply2npy.py']
 )
